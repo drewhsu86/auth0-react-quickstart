27ebcf888c6b871d12924d4ec3a338cbb7a813c3
drewhsu86 <bowserhsu@gmail.com>
2020-10-16 19:13:48 -0400

    mainly worked on rules and removed requirement for verified email to have a whitelisted tester account

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -4,7 +4,8 @@ import { useAuth0 } from '@auth0/auth0-react'
 import APIGetter from './APIGetter'
 
 export default function RulesViewer() {
-  const { user, isAuthenticated, isLoading } = useAuth0()
+  const { user, isAuthenticated, isLoading, logout } = useAuth0()
+  console.log(user)
   if (isLoading) {
     return <div>
       <h1>Loading...</h1>
@@ -13,12 +14,11 @@ export default function RulesViewer() {
   if (!isAuthenticated) {
     return <div>
       <h1>You did not successfully Login.</h1>
-      <Link to="/">
+      <Link to="/" onClick={() => logout({ returnTo: 'http://localhost:3000' })}>
           Return to Login Screen
       </Link>
     </div>
   }
-  
   return (
     <div>
       <h1>Logged in as {user.email}.</h1>
