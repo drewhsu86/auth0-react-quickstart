4378ab4d9b43a81ad0fe7b09bf1a49a51109c103
drewhsu86 <bowserhsu@gmail.com>
2020-10-16 20:11:59 -0400

    made viewer component a state variable

--- src/components/APIGetter.jsx
+++ src/components/APIGetter.jsx
@@ -1,35 +1,46 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
 
-export default function RulesGetter() {
+export default class RulesGetter extends Component {
 
-  const [ruleList, setRuleList] = useState([]);
-  const [appList, setAppList] = useState([]);
-  const [errorText, setErrorText] = useState('');
+  constructor() {
+    super()
+    this.state = {
+      ruleList: [],
+      appList: [],
+      errorText: ''
+    }
+  }
 
   // if this component is successfully called, use m2m app to get api token
   // then use api token to get rules from management api v2
-  useEffect(() => {
-    initialize()
-  }, [])
+  componentDidMount() {
+    this.initialize()
+  }
 
-  async function initialize() {
+  setStateAs(data, stateName) {
+    this.setState({
+      [stateName]: data
+    })
+  }
+
+  initialize = async () => {
     try {
-      const accessToken = await getAPIToken()
-      setErrorText('Successfully received API JWT')
+      const accessToken = await this.getAPIToken()
+      this.setStateAs('Successfully received API JWT', 'errorText')
 
-      const rules = await getFromAPI(accessToken, 'rules')
-      setRuleList(rules)
+      const rules = await this.getFromAPI(accessToken, 'rules')
+      this.setStateAs(rules, 'ruleList')
 
-      const apps = await getFromAPI(accessToken, 'clients')
-      setAppList(apps)
+      const apps = await this.getFromAPI(accessToken, 'clients')
+      this.setStateAs(apps, 'appList')
 
     } catch (error) {
       console.error(error)
     }
   }
 
-  async function getAPIToken() {
+  getAPIToken = async () => {
     try {
       // axios post values from: https://auth0.com/docs/tokens/management-api-access-tokens/get-management-api-access-tokens-for-production
       
@@ -48,11 +59,11 @@ export default function RulesGetter() {
       return response.data.access_token 
     } catch (error) {
       console.error(error)
-      setErrorText('See console: Error while getting API JWT')
+      this.setStateAs('See console: Error while getting API JWT', 'errorText')
     }
   }
 
-  async function getFromAPI(accessToken, path) {
+  getFromAPI = async (accessToken, path) => {
     try {
       const headers = {
         Authorization: 'Bearer ' + accessToken
@@ -63,36 +74,38 @@ export default function RulesGetter() {
       return response.data
     } catch (error) {
       console.error(error)
-      setErrorText('See console: Error while accessing API')
+      this.setStateAs('See console: Error while accessing API', 'errorText')
     }
   }
   
-  return (
-    <div>
-      {errorText ? <h3>{errorText}</h3> : null}
+  render() {
+    return (
+      <div>
+        {this.state.errorText ? <h3>{this.state.errorText}</h3> : null}
 
-      <h1>Apps List:</h1>
-      {
-        appList.map((app, idx) => {
-          return <div key={idx} style={{ border: "1px solid black", padding: "10px", margin: "0 20%" }}>
-            <h2> {app.name} </h2>  
-          </div>
-        })
-      }
+        <h1>Apps List:</h1>
+        {
+          this.state.appList.map((app, idx) => {
+            return <div key={idx} style={{ border: "1px solid black", padding: "10px", margin: "0 20%" }}>
+              <h2> {app.name} </h2>
+            </div>
+          })
+        }
 
-      <h1>Rules List:</h1>
-      {
-        ruleList.map(rule => {
-          return <div key={rule.id} style={{ border: "1px solid black", padding: "10px", margin: "0 20%" }}>
-            <h2> {rule.name} </h2>
-            <p>Order: {rule.order}</p>
-            <p>Stage: {rule.stage}</p>
-            <p style={{ overflow: "auto", backgroundColor: "lightgray", width: "80%", height: "100px", margin: "auto", padding: "5px" }}>
-              {rule.script}
-            </p>
-          </div>
-        })
-      }
-    </div>
-  )
+        <h1>Rules List:</h1>
+        {
+          this.state.ruleList.map(rule => {
+            return <div key={rule.id} style={{ border: "1px solid black", padding: "10px", margin: "0 20%" }}>
+              <h2> {rule.name} </h2>
+              <p>Order: {rule.order}</p>
+              <p>Stage: {rule.stage}</p>
+              <p style={{ overflow: "auto", backgroundColor: "lightgray", width: "80%", height: "100px", margin: "auto", padding: "5px" }}>
+                {rule.script}
+              </p>
+            </div>
+          })
+        }
+      </div>
+    )
+  }
 }
